b2f5ffe656e124ea1a051306e5825c0d461b7047
Tony Bowden <tony@tmtm.com>
2016-02-28 07:17:13 +0000

    Scrape in smaller chunks

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,7 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/germany-bundestag-members-wikipedia', column: 'identifier__wikipedia_de')
-EveryPolitician::Wikidata.scrape_wikidata(names: { de: names })
-warn EveryPolitician::Wikidata.notify_rebuilder
+names.each_slice(50) do |sliced|
+  EveryPolitician::Wikidata.scrape_wikidata(names: { de: sliced })
+end
 
