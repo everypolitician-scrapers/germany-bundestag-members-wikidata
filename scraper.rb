e820258aed10639e8c5a2e5fc7ee42271fcc0610
Tony Bowden <tony@tmtm.com>
2016-03-17 20:56:05 +0100

    Change column to look up

--- scraper.rb
+++ scraper.rb
@@ -3,8 +3,8 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/germany-bundestag-members-wikipedia', column: 'identifier__wikipedia_de')
-names.each_slice(50) do |sliced|
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/germany-bundestag-members-wikipedia', column: 'wikiname')
+names.each_slice(50).shuffle do |sliced|
   EveryPolitician::Wikidata.scrape_wikidata(names: { de: sliced })
 end
 
