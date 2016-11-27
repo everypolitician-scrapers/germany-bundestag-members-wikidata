2585e8f632558a99a8ef225f88f1bf53961b948f
Tony Bowden <tony@tmtm.com>
2016-11-27 11:01:24 +0000

    Remove batch processing

--- scraper.rb
+++ scraper.rb
@@ -7,5 +7,5 @@ require 'wikidata/fetcher'
 existing = EveryPolitician::Index.new.country("Germany").lower_house.popolo.persons.map(&:wikidata).compact
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/germany-bundestag-members-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { de: names }, batch_size: 20)
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { de: names })
 
