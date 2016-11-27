a8362de804fc212aafd227c87db97b92e587140b
Tony Bowden <tony@tmtm.com>
2016-11-27 11:01:15 +0000

    Fetch existing data from EveryPolitician

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,11 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
+existing = EveryPolitician::Index.new.country("Germany").lower_house.popolo.persons.map(&:wikidata).compact
+
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/germany-bundestag-members-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { de: names }, batch_size: 20)
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { de: names }, batch_size: 20)
 
