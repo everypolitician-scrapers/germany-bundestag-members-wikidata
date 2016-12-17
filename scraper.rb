205202dd81af726d1eacd9edf2f0302669c62303
Tony Bowden <tony@tmtm.com>
2016-12-17 09:55:11 +0000

    Only fetch by name
    See if this uses less memory

--- scraper.rb
+++ scraper.rb
@@ -4,9 +4,6 @@
 require 'everypolitician'
 require 'wikidata/fetcher'
 
-existing = EveryPolitician::Index.new.country("Germany").lower_house.popolo.persons.map(&:wikidata).compact
-
+# existing = EveryPolitician::Index.new.country("Germany").lower_house.popolo.persons.map(&:wikidata).compact
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/germany-bundestag-members-wikipedia', column: 'wikiname')
-warn "Fetching #{existing.count} ids and #{names.count} names"
-EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { de: names }, batch_size: 1000)
-
+EveryPolitician::Wikidata.scrape_wikidata(names: { de: names })
