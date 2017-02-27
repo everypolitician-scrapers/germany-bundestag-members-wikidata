ffcc641d53e7edeb751152f4100a0f70f31ecd4d
Tony Bowden <tony@tmtm.com>
2017-02-27 09:47:00 +0000

    Also get all P39 Members
    Use SPARQL to get everyone with a P39 Membership of the Bundestag

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,10 @@
 require 'everypolitician'
 require 'wikidata/fetcher'
 
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q1939555 . }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
 # existing = EveryPolitician::Index.new.country("Germany").lower_house.popolo.persons.map(&:wikidata).compact
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/germany-bundestag-members-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { de: names }, batch_size: 250)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { de: names }, batch_size: 250)
