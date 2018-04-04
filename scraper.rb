9fbf96a6525bebaaea5c4e783d23beb20fbee9fa
Tony Bowden <tony@tmtm.com>
2018-04-04 15:39:15 +0100

    Fetch all P39 memberships, not only wdt: ones
    wdt:P39 won't find anyone who has a member of the Bundestag with normal
    rank, but some other membership as preferred (e.g. head of a party)

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,7 @@
 require 'everypolitician'
 require 'wikidata/fetcher'
 
-sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q1939555 . }'
+sparq = 'SELECT ?item WHERE { ?item p:P39/ps:P39 wd:Q1939555 . }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
 # existing = EveryPolitician::Index.new.country("Germany").lower_house.popolo.persons.map(&:wikidata).compact
