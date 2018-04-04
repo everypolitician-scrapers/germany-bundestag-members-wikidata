3d0f8e9f8c835f0d358db0d76cd9f5870874a474
Tony Bowden <tony@tmtm.com>
2018-04-04 15:51:31 +0100

    Remove obsolete lookup of existing matches
    We're not using this, so remove it, rather than just commenting it out.

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,11 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'everypolitician'
 require 'wikidata/fetcher'
 
 sparq = 'SELECT ?item WHERE { ?item p:P39/ps:P39 wd:Q1939555 . }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
-# existing = EveryPolitician::Index.new.country("Germany").lower_house.popolo.persons.map(&:wikidata).compact
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/germany-bundestag-members-wikipedia', column: 'wikiname')
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { de: names }, batch_size: 100)
